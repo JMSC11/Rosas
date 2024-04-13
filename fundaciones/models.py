90fd32ca935a72930c3b1abb9c9fbf5df1589d95
MisaelSc <misaelsc.ms@gmail.com>
2024-04-13 00:17:51 -0600

    Agregando modelo Adolescentes

--- fundaciones/models.py
+++ fundaciones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Fundacion(models.Model):
@@ -12,6 +13,7 @@ class Fundacion(models.Model):
     telefono = models.CharField(max_length=20, blank=True)
     pagina_web = models.URLField(max_length=200, blank=True)
     correo_electronico = models.EmailField(max_length=100, blank=True)
-
+    gestor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='gestor_fundacion')
+    
     def __str__(self):
         return self.nombre_fundacion
